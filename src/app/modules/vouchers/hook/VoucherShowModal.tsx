12d7f3b66eacd252a44edd1c69cf597c0f7603c3
Boclair Temgoua <temgoua2000@gmail.com>
2022-06-24 17:32:57 +0200

    fix(tests): fix controlle voucher

--- src/app/modules/vouchers/hook/VoucherShowModal.tsx
+++ src/app/modules/vouchers/hook/VoucherShowModal.tsx
@@ -76,7 +76,7 @@ export const VoucherShowModal: React.FC<Props> = ({ setOpenModal, voucherItem })
         aria-modal='true'
       >
         {/* begin::Modal dialog */}
-        <div className='modal-dialog modal-dialog-centered mw-750px modal-dialog-scrollable'>
+        <div className='modal-dialog modal-dialog-centered mw-1000px modal-dialog-scrollable'>
           {/* begin::Modal content */}
           <div className='modal-content'>
             <div className="modal-header pb-0 border-0 justify-content-end">
