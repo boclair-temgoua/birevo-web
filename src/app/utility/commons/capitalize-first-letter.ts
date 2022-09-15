522e7f3b5bc98108f312a429b8c2d4579bd8e5da
Boclair Temgoua <temgoua2000@gmail.com>
2022-09-15 15:11:03 +0200

    fix(general): fix bug

--- src/app/utility/commons/capitalize-first-letter.ts
+++ src/app/utility/commons/capitalize-first-letter.ts
@@ -4,6 +4,7 @@ export const capitalizeFirstLetter = (firstItem: string, secondItem: string) => 
   return (capitalizeName(firstItem).charAt(0) + capitalizeName(secondItem).charAt(0)).toUpperCase()
 }
 
+/** Fix date */
 export const capitalizeOneFirstLetter = (fullItem: string) => {
   return capitalizeName(fullItem).charAt(0).toUpperCase()
 }
