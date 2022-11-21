66e7f8763d51542fd924415940ea115013a82d8d
Boclair Temgoua <temgoua2000@gmail.com>
2022-11-21 19:54:04 +0100

    fix(general): fix organizaion info

--- src/app/pages/dashboard/DashboardWrapper.tsx
+++ src/app/pages/dashboard/DashboardWrapper.tsx
@@ -24,7 +24,7 @@ const DashboardPage: FC = () => (
   <>
     {/* begin::Row */}
     <div className='row gy-5 g-xl-8'>
-      <div className='col-xxl-4'>
+      {/* <div className='col-xxl-6'>
         <MixedWidget2
           className='card-xl-stretch mb-xl-8'
           chartColor='danger'
@@ -32,10 +32,10 @@ const DashboardPage: FC = () => (
           strokeColor='#cb1e46'
         />
       </div>
-      <div className='col-xxl-4'>
+      <div className='col-xxl-6'>
         <ListsWidget5 className='card-xxl-stretch' />
-      </div>
-      <div className='col-xxl-4'>
+      </div> */}
+      {/* <div className='col-xxl-4'>
         <MixedWidget10
           className='card-xxl-stretch-50 mb-5 mb-xl-8'
           chartColor='primary'
@@ -46,23 +46,24 @@ const DashboardPage: FC = () => (
           chartColor='primary'
           chartHeight='175px'
         />
-      </div>
+      </div> */}
     </div>
     {/* end::Row */}
 
     {/* begin::Row */}
     <div className='row gy-5 gx-xl-8'>
-      <div className='col-xxl-4'>
+      {/* <div className='col-xxl-4'>
         <ListsWidget3 className='card-xxl-stretch mb-xl-3' />
-      </div>
-      <div className='col-xl-8'>
+      </div> */}
+      {/* <div className='col-xl-8'>
         <TablesWidget10 className='card-xxl-stretch mb-5 mb-xl-8' />
-      </div>
+      </div> */}
+      {/* <TablesWidget10 className='card-xxl-stretch mb-5 mb-xl-8' /> */}
     </div>
     {/* end::Row */}
 
     {/* begin::Row */}
-    <div className='row gy-5 g-xl-8'>
+    {/* <div className='row gy-5 g-xl-8'>
       <div className='col-xl-4'>
         <ListsWidget2 className='card-xl-stretch mb-xl-8' />
       </div>
@@ -71,12 +72,11 @@ const DashboardPage: FC = () => (
       </div>
       <div className='col-xl-4'>
         <ListsWidget4 className='card-xl-stretch mb-5 mb-xl-8' items={5} />
-        {/* partials/widgets/lists/_widget-4', 'class' => 'card-xl-stretch mb-5 mb-xl-8', 'items' => '5' */}
       </div>
-    </div>
+    </div> */}
     {/* end::Row */}
 
-    <div className='row g-5 gx-xxl-8'>
+    {/* <div className='row g-5 gx-xxl-8'>
       <div className='col-xxl-4'>
         <MixedWidget8
           className='card-xxl-stretch mb-xl-3'
@@ -87,7 +87,7 @@ const DashboardPage: FC = () => (
       <div className='col-xxl-8'>
         <TablesWidget5 className='card-xxl-stretch mb-5 mb-xxl-8' />
       </div>
-    </div>
+    </div> */}
   </>
 )
 
